184290be89deb2b5b3d3f346122e9b55a074037d
RickyDavinci <ricknoutat@gmail.com>
2024-06-06 02:36:18 +0100

    page transition

--- components/PageTransition.tsx
+++ components/PageTransition.tsx
@@ -1,11 +1,34 @@
+"use client";
+
 import { ReactNode } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation.js";
 
 type PageTransitionProps = {
   children: ReactNode;
 };
 
 const PageTransition = ({ children }: PageTransitionProps) => {
-  return <div className="page-transition">{children}</div>;
+  const pathname = usePathname();
+  return (
+    <AnimatePresence>
+      <div key={pathname}>
+        <motion.div
+          initial={{ opacity: 1 }}
+          animate={{
+            opacity: 0,
+            transition: {
+              delay: 1,
+              duration: 0.4,
+              ease: "easeInOut",
+            },
+          }}
+          className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
+        ></motion.div>
+        {children}
+      </div>
+    </AnimatePresence>
+  );
 };
 
 export default PageTransition;
