da5fe46d3852eb28cb5ac3ee38d5a670dd3d179b
RickyDavinci <ricknoutat@gmail.com>
2024-06-08 01:09:22 +0100

    ajout des assets

---
+++ components/Social.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link.js";
+import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter } from "react-icons/fa";
+
+const socials = [
+  { icon: <FaGithub />, path: "" },
+  { icon: <FaLinkedinIn />, path: "" },
+  { icon: <FaTwitter />, path: "" },
+  { icon: <FaYoutube />, path: "" },
+];
+
+type SocialProps = {
+  containerStyles: string;
+  iconStyles: string;
+};
+
+export const Social = ({ containerStyles, iconStyles }: SocialProps) => {
+  return (
+    <div className={containerStyles}>
+      {socials.map((item, index) => {
+        return (
+          <Link key={index} href={item.path} className={iconStyles}>
+            {item.icon}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
